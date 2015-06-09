56556fae14551924aba449e8fc6e78fa777a0a51
foray1010 <foray1010@gmail.com>
2015-06-09 20:02:47 +0800

    Add eslint-plugin-react to lint jsx code

--- js/_components/item.js
+++ js/_components/item.js
@@ -25,7 +25,7 @@ function render({props, state}) {
       onDragEnd={dragEndHandler}
       onDragOver={dragOverHandler}
       onDragStart={dragStartHandler}>
-      <img class='icon' src={''} alt='' draggable='false'>
+      <img class='icon' src={''} alt='' draggable='false' />
       <span>{''}</span>
     </p>
   );
