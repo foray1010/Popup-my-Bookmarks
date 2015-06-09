693b117420cf0efa03b2ab36b2d02cec66caa7dc
foray1010 <foray1010@users.noreply.github.com>
2015-06-10 00:02:53 +0800

    Disable scrolling arrows && Add click and hover handler to item

--- js/_components/popup/headbox.js
+++ js/_components/popup/headbox.js
@@ -1,10 +1,14 @@
 import {element} from 'deku';
 
+function closeHandler(event, {props, state}, updateState) {
+
+}
+
 function render({props, state}) {
   return (
     <div class='headbox'>
       <div class='head-title no-text-overflow'>{''}</div>
-      <div class='head-close' />
+      <div class='head-close' onClick={closeHandler} />
     </div>
   );
 }
