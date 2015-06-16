1ac8273561fa3c110fa05910d5891d4a6e7f58ad
foray1010 <foray1010@users.noreply.github.com>
2015-06-16 18:20:08 +0800

    Move menu's children logic from item.js to menu.js

--- js/_components/popup/menu.js
+++ js/_components/popup/menu.js
@@ -2,18 +2,43 @@ import {element} from 'deku';
 
 const _getMsg = chrome.i18n.getMessage;
 
-const menuPattern = [
-  [],
-  [],
-  ['cut', 'copy', 'paste'],
-  ['addPage', 'addFolder', 'addSeparator'],
-  ['sortByName']
-];
+function getChildrenHiddenStatus(itemInfo) {
+  let childrenHiddenStatus = [];
+
+  switch (globals.getBookmarkType(itemInfo)) {
+    case 'folder':
+    case 'bkmark':
+      if (globals.isRootFolder(itemInfo)) {
+        childrenHiddenStatus = [false, true, true, true, true];
+      // } else if (!IS_SEARCHING) {
+      //   childrenHiddenStatus = [false, false, false, false, false];
+      } else {
+        childrenHiddenStatus = [false, false, false, true, true];
+      }
+      break;
+
+    case 'no-bkmark':
+      childrenHiddenStatus = [true, true, false, false, true];
+  }
+
+  return childrenHiddenStatus;
+}
 
 function render({props, state}) {
   const itemInfo = props.menuTarget;
 
+  let menuItems;
+
   if (itemInfo) {
+    const childrenHiddenStatus = getChildrenHiddenStatus(itemInfo);
+    const menuPattern = [
+      [],
+      [],
+      ['cut', 'copy', 'paste'],
+      ['addPage', 'addFolder', 'addSeparator'],
+      ['sortByName']
+    ];
+
     if (globals.isFolder(itemInfo)) {
       menuPattern[0] = ['openAll', 'openAllInN', 'openAllInI'];
       menuPattern[1] = ['rename', 'del'];
@@ -21,20 +46,20 @@ function render({props, state}) {
       menuPattern[0] = ['openInB', 'openInN', 'openInI'];
       menuPattern[1] = ['edit', 'del'];
     }
-  }
 
-  const menuItems = menuPattern.map((menuAreaKeys, menuAreaIndex) => {
-    const isMenuAreaHidden = props.menuHideChild[menuAreaIndex];
-    const menuAreaItems = menuAreaKeys.map((menuItemKey) => {
-      return (
-        <p class='item menu-item'>
-          {_getMsg(menuItemKey)}
-        </p>
-      );
+    menuItems = menuPattern.map((menuAreaKeys, menuAreaIndex) => {
+      const isMenuAreaHidden = childrenHiddenStatus[menuAreaIndex];
+      const menuAreaItems = menuAreaKeys.map((menuItemKey) => {
+        return (
+          <p class='item menu-item'>
+            {_getMsg(menuItemKey)}
+          </p>
+        );
+      });
+
+      return <div hidden={isMenuAreaHidden}>{menuAreaItems}</div>;
     });
-
-    return <div hidden={isMenuAreaHidden}>{menuAreaItems}</div>;
-  });
+  }
 
   return <div id='menu' hidden={props.isHidden}>{menuItems}</div>;
 }
