e5610dc3201d22eca7bbea7d1933366ed4012ab3
foray1010 <foray1010@users.noreply.github.com>
2015-06-17 16:04:51 +0800

    Init menu item click event

--- js/_components/popup/menu.js
+++ js/_components/popup/menu.js
@@ -2,6 +2,13 @@ import {element} from 'deku';
 
 const _getMsg = chrome.i18n.getMessage;
 
+function closeMenu() {
+  globals.setRootState({
+    hiddenMenu: true,
+    hiddenMenuCover: true
+  });
+}
+
 function getChildrenHiddenStatus(itemInfo) {
   let childrenHiddenStatus = [];
 
@@ -22,6 +29,47 @@ function getChildrenHiddenStatus(itemInfo) {
   }
 
   return childrenHiddenStatus;
+}
+
+function getMenuItemNum(menuItem) {
+  const menuItemList = document.getElementsByClassName('menu-item');
+
+  return Array.prototype.indexOf.call(menuItemList, menuItem);
+}
+
+function menuClickEvent(event) {
+  const target = event.target;
+
+  const menuItemNum = getMenuItemNum(target);
+
+  switch (menuItemNum) {
+    case 0: // Open bookmark(s) in background tab or this window
+    case 1: // in new window
+    case 2: // in incognito window
+      break;
+
+    case 3: // Edit... or Rename...
+    case 9: // Add folder...
+      return true;
+
+    case 4: // Delete
+      break;
+
+    case 5: // Cut
+    case 6: // Copy
+    case 7: // Paste
+      break;
+
+    case 8: // Add current page
+      break;
+
+    case 10: // Add separator
+      break;
+
+    case 11: // Sort by name
+  }
+
+  closeMenu();
 }
 
 function render({props, state}) {
@@ -51,7 +99,9 @@ function render({props, state}) {
       const isMenuAreaHidden = childrenHiddenStatus[menuAreaIndex];
       const menuAreaItems = menuAreaKeys.map((menuItemKey) => {
         return (
-          <p class='item menu-item'>
+          <p
+            class='item menu-item'
+            onClick={menuClickEvent}>
             {_getMsg(menuItemKey)}
           </p>
         );
