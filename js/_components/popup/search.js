44636b19f08769ffc8943a02faf5bebb0db1a9d4
foray1010 <foray1010@users.noreply.github.com>
2015-06-24 21:02:05 +0800

    Fix search box weird display when font size is larger than default

--- js/_components/popup/search.js
+++ js/_components/popup/search.js
@@ -8,6 +8,7 @@ function inputHandler(event, {props, state}) {
 function render({props, state}) {
   return (
     <div id='search-box'>
+      <img id='search-img' src='/img/search.png' alt='' />
       <input
         id='search-input'
         type='search'
