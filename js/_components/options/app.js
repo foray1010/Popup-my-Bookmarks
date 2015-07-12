e0313f6bd82a3c2759af91999dfc8d8332867552
foray1010 <foray1010@users.noreply.github.com>
2015-07-12 09:55:19 +0800

    Init options.js

---
+++ js/_components/options/app.js
@@ -0,0 +1,18 @@
+import {element} from 'deku';
+
+function initialState(props) {
+  return {
+    storage: props.storage
+  };
+}
+
+function render({props, state}, setState) {
+  globals.setRootState = setState;
+
+  return (
+    <div id='app'>
+    </div>
+  );
+}
+
+export default {initialState, render};
---
+++ js/options.js
@@ -0,0 +1,18 @@
+import {element, render, tree} from 'deku';
+
+import App from './_components/options/app';
+
+window.globals = {};
+
+new Promise((resolve) => {
+  chrome.storage.sync.get(null, (storage) => {
+    resolve(storage);
+  });
+})
+  .then((storage) => {
+    const app = tree(
+      <App storage={storage} />
+    );
+
+    render(app, document.getElementById('container'));
+  });
