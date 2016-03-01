328a7dc63f17a3e6e769ecee3187e5589c27a6c9
foray1010 <foray1010@users.noreply.github.com>
2016-03-01 22:34:06 +0800

    Improve webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@
 
 const webpack = require('webpack')
 
-const isDev = process.env.NODE_ENV === 'development'
 const webpackPlugins = [
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
@@ -12,28 +11,42 @@ const webpackPlugins = [
   new webpack.optimize.OccurenceOrderPlugin()
 ]
 
-if (!isDev) {
-  webpackPlugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      drop_console: true,
-      pure_getters: true,
-      unsafe: true,
-      warnings: false
-    },
-    output: {
-      comments: false,
-      screw_ie8: true
-    }
-  }))
+let devtool = undefined
+let isWatch = false
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    devtool = 'source-map'
+    isWatch = true
+    break
+
+  case 'production':
+    webpackPlugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          drop_console: true,
+          pure_getters: true,
+          unsafe: true,
+          warnings: false
+        },
+        output: {
+          comments: false,
+          screw_ie8: true
+        }
+      })
+    )
+    break
+
+  default:
 }
 
 module.exports = {
-  devtool: isDev ? 'source-map' : undefined,
+  devtool: devtool,
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /\/node_modules\//,
         loader: 'babel-loader'
       }
     ]
@@ -50,5 +63,5 @@ module.exports = {
     timings: true,
     version: false
   },
-  watch: isDev
+  watch: isWatch
 }
