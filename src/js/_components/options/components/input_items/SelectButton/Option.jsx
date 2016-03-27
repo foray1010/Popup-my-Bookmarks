5beb3eacf885960313ddf3b1b13fb68da7dbd0eb
foray1010 <foray1010@users.noreply.github.com>
2016-03-27 20:54:34 +0800

    Handle options by Option.jsx

---
+++ src/js/_components/options/components/input_items/SelectButton/Option.jsx
@@ -0,0 +1,72 @@
+import {autobind} from 'core-decorators'
+import {connect} from 'react-redux'
+import {createElement, Component, PropTypes} from 'react'
+import classNames from 'classnames'
+
+import {updateSingleOption} from '../../../actions'
+
+const msgNo = chrome.i18n.getMessage('opt_no')
+const msgYes = chrome.i18n.getMessage('opt_yes')
+
+const mapStateToProps = (state) => ({
+  options: state.options
+})
+
+class Option extends Component {
+  @autobind
+  handleChange(evt) {
+    const {
+      dispatch,
+      optionName
+    } = this.props
+
+    const newOptionValue = evt.target.value === 'true'
+
+    dispatch(updateSingleOption(optionName, newOptionValue))
+  }
+
+  render() {
+    const {
+      optionChoice,
+      optionName,
+      options
+    } = this.props
+
+    const optionValue = options[optionName]
+
+    const buttonText = optionChoice ? msgYes : msgNo
+    const isChecked = optionValue === optionChoice
+
+    const selectButtonClassName = classNames(
+      'select-button-item',
+      {
+        'select-button-item-active': isChecked
+      }
+    )
+
+    return (
+      <label className={selectButtonClassName}>
+        <input
+          name={optionName}
+          type='radio'
+          value={String(optionChoice)}
+          checked={isChecked}
+          hidden
+          onChange={this.handleChange}
+        />
+        {buttonText}
+      </label>
+    )
+  }
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  Option.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    optionChoice: PropTypes.bool.isRequired,
+    optionName: PropTypes.string.isRequired,
+    options: PropTypes.object.isRequired
+  }
+}
+
+export default connect(mapStateToProps)(Option)
---
+++ src/js/_components/options/components/input_items/SelectMultiple/Option.jsx
@@ -0,0 +1,74 @@
+import {autobind} from 'core-decorators'
+import {connect} from 'react-redux'
+import {createElement, Component, PropTypes} from 'react'
+
+import {updateSingleOption} from '../../../actions'
+
+const mapStateToProps = (state) => ({
+  options: state.options
+})
+
+class Option extends Component {
+  @autobind
+  handleChange(evt) {
+    const {
+      dispatch,
+      optionName,
+      options
+    } = this.props
+
+    const newOptionValue = options[optionName].asMutable()
+    const targetValue = parseInt(evt.target.value, 10)
+
+    const targetValueIndex = newOptionValue.indexOf(targetValue)
+
+    const wasChecked = targetValueIndex >= 0
+
+    if (wasChecked) {
+      newOptionValue.splice(targetValueIndex, 1)
+    } else {
+      newOptionValue.push(targetValue)
+      newOptionValue.sort()
+    }
+
+    dispatch(updateSingleOption(optionName, newOptionValue))
+  }
+
+  render() {
+    const {
+      optionChoice,
+      optionChoiceIndex,
+      optionName,
+      options
+    } = this.props
+
+    const optionValue = options[optionName]
+
+    const isChecked = optionValue.includes(optionChoiceIndex)
+
+    return (
+      <label>
+        <input
+          name={optionName}
+          type='checkbox'
+          value={String(optionChoiceIndex)}
+          checked={isChecked}
+          onChange={this.handleChange}
+        />
+        {optionChoice}
+      </label>
+    )
+  }
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  Option.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    optionChoice: PropTypes.string.isRequired,
+    optionChoiceIndex: PropTypes.number.isRequired,
+    optionName: PropTypes.string.isRequired,
+    options: PropTypes.object.isRequired
+  }
+}
+
+export default connect(mapStateToProps)(Option)
