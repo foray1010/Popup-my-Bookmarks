eee5afaf31785ab33c84c495b35b0e43e6f5335a
foray1010 <foray1010@users.noreply.github.com>
2016-09-24 20:18:50 +0800

    Add script to generate locales messages from transifex

---
+++ scripts/generateLocalesFromTransifex.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const _ = require('lodash')
+const bluebird = require('bluebird')
+const co = require('co')
+const fs = require('fs-extra')
+const path = require('path')
+const prompt = require('prompt')
+const Transifex = require('transifex')
+
+bluebird.promisifyAll(fs)
+bluebird.promisifyAll(prompt)
+
+const schema = {
+  properties: {
+    transifex_username: {
+      description: 'transifex_username (you can register one for free)',
+      required: true
+    },
+    transifex_password: {
+      hidden: true,
+      required: true
+    }
+  }
+}
+
+const projectSlug = 'popup-my-bookmarks'
+const resourceSlug = 'messagesjson-1'
+
+const localesPath = path.join('src', '_locales')
+
+co(function* () {
+  prompt.start()
+  const cred = yield prompt.getAsync(schema)
+
+  const transifex = new Transifex({
+    credential: `${cred.transifex_username}:${cred.transifex_password}`
+  })
+  bluebird.promisifyAll(transifex)
+
+  const allAvailableLanguages = yield transifex.statisticsMethodsAsync(
+    projectSlug,
+    resourceSlug
+  )
+  for (const availableLanguage of Object.keys(allAvailableLanguages)) {
+    console.log('\n' + '='.repeat(10))
+    console.log(`working on "${availableLanguage}"`)
+
+    yield fs.mkdirsAsync(
+      path.join(localesPath, availableLanguage)
+    )
+
+    const messagesJsonStr = yield transifex.translationInstanceMethodAsync(
+      projectSlug,
+      resourceSlug,
+      availableLanguage,
+      {mode: 'onlytranslated'}
+    )
+    const messagesJson = JSON.parse(messagesJsonStr)
+
+    const sortedMessagesJson = Object.keys(messagesJson)
+      .filter((key) => Boolean(_.get(messagesJson, `${key}.message`)))
+      .sort()
+      .reduce((obj, key) => {
+        obj[key] = messagesJson[key]
+        return obj
+      }, {})
+
+    yield fs.outputJsonAsync(
+      path.join(localesPath, availableLanguage, 'messages.json'),
+      sortedMessagesJson,
+      {spaces: '\t'}
+    )
+
+    console.log(`"${availableLanguage}" is completed`)
+  }
+}).catch((err) => console.error(err.stack))
