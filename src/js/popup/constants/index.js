3536cb727bc2135c31978b313944120f06f6f977
foray1010 <foray1010@users.noreply.github.com>
2016-12-27 00:34:44 +0800

    Use flow + tcomb

--- src/js/popup/constants/index.js
+++ src/js/popup/constants/index.js
@@ -1,13 +1,15 @@
+/* @flow */
+
 export * from '../../common/constants'
 export * from './actionTypes'
 
-export const DRAG_INDICATOR = 'drag-indicator'
-export const GOLDEN_GAP = 2
-export const MAX_HEIGHT = 596
-export const SEPARATE_THIS_URL = 'http://separatethis.com/'
+export const DRAG_INDICATOR: string = 'drag-indicator'
+export const GOLDEN_GAP: number = 2
+export const MAX_HEIGHT: number = 596
+export const SEPARATE_THIS_URL: string = 'http://separatethis.com/'
 
-export const TYPE_BOOKMARK = 'bookmark'
-export const TYPE_FOLDER = 'folder'
-export const TYPE_NO_BOOKMARK = 'no-bookmark'
-export const TYPE_ROOT_FOLDER = 'root-folder'
-export const TYPE_SEPARATOR = 'separator'
+export const TYPE_BOOKMARK: string = 'bookmark'
+export const TYPE_FOLDER: string = 'folder'
+export const TYPE_NO_BOOKMARK: string = 'no-bookmark'
+export const TYPE_ROOT_FOLDER: string = 'root-folder'
+export const TYPE_SEPARATOR: string = 'separator'
