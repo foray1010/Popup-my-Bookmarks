28c4bb4147b1c512323bd62553cc3d64da2ff53a
foray1010 <foray1010@users.noreply.github.com>
2017-01-16 00:30:36 +0800

    Ask for fork this repo or donate via Bitcoin in options page

--- src/js/options/components/Donate.jsx
+++ src/js/options/components/Donate.jsx
@@ -15,27 +15,54 @@ const Donate = () => (
     >
       <img src='/img/btn_donateCC_LG.png' alt='' />
     </a>
-    <p styleName='desc'>
-      {'If you like Popup my Bookmarks, please rate this extension on '}
-      <a
-        styleName='link'
-        href='http://goo.gl/x9Wlq'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Chrome Web Store
-      </a>
-      {' and consider to buy me a drink via '}
-      <a
-        styleName='link'
-        href={paypalUrl}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        PayPal
-      </a>
-      {'!'}
-    </p>
+    <section styleName='desc'>
+      If you like Popup my Bookmarks, please consider to:
+      <ol>
+        <li>
+          rate it on&nbsp;
+          <a
+            styleName='link'
+            href='http://goo.gl/x9Wlq'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Chrome Web Store
+          </a>
+        </li>
+        <li>
+          fork me on&nbsp;
+          <a
+            styleName='link'
+            href='https://github.com/foray1010/Popup-my-Bookmarks'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          buy me a drink via&nbsp;
+          <a
+            styleName='link'
+            href={paypalUrl}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            PayPal
+          </a>
+          &nbsp;or&nbsp;
+          <a
+            styleName='link'
+            href='bitcoin:16sJwjWsUWrFn5iAGGBCiQbqUD4Pb5YdDR'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Bitcoin
+          </a>
+          &nbsp;:)
+        </li>
+      </ol>
+    </section>
   </footer>
 )
 
