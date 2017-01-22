609546e11bd2b0f7f1d615f6fa6b99686c4084c8
foray1010 <foray1010@users.noreply.github.com>
2017-01-23 00:04:02 +0800

    First draft of options.Contributors

--- src/js/options/components/Contributors.jsx
+++ src/js/options/components/Contributors.jsx
@@ -1,7 +1,66 @@
 import {createElement} from 'react'
+import {static as Immutable} from 'seamless-immutable'
+import CSSModules from 'react-css-modules'
+
+import styles from '../../../css/options/contributors.css'
+
+const contributors = Immutable({
+  Developer: [
+    'foray1010'
+  ],
+  Tester: [
+    'David Bryant'
+  ],
+  'Dutch Translator': [
+    'Marzas'
+  ],
+  'French Translator': [
+    'foXaCe',
+    'Alexis Schapman'
+  ],
+  'German Translator': [
+    'Gürkan ZENGIN'
+  ],
+  'Italian Translator': [
+    'Giacomo Fabio Leone'
+  ],
+  'Korean Translator': [
+    'zenyr'
+  ],
+  'Norwegian Bokmål Translator': [
+    'Bjorn Tore Asheim'
+  ],
+  'Russian Translator': [
+    'kameo',
+    'Oleg K,'
+  ],
+  'Vietnamese Translator': [
+    'Anh Phan'
+  ],
+  Donor: [
+    'Abtin Samadi',
+    'Drake Roman',
+    'j**************r@u******.nl',
+    'Claudine J Haddock',
+    'Carlos Velazquez',
+    'Jacob Randall',
+    'Николай Кондрашов',
+    'a******s@v********.ca',
+    'L. Smith',
+    'j*********e@g****.com',
+    'J*****y@c*.com'
+  ]
+})
 
 const Contributors = () => (
-  <main />
+  <dl styleName='main'>
+    {Object.keys(contributors).map((contributeType) => ([
+      <dt>{contributeType}</dt>,
+      ...contributors[contributeType].map((contributor) => (
+        <dd>{contributor}</dd>
+      ))
+    ]))}
+  </dl>
 )
 
-export default Contributors
+export default CSSModules(Contributors, styles)
