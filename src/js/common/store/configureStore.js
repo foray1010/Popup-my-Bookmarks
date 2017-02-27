5bf943302a80c01bfe7ca7e2bd72c8869cefa529
foray1010 <foray1010@users.noreply.github.com>
2017-02-27 22:32:15 +0800

    Remove annoying redux logger

--- src/js/common/store/configureStore.js
+++ src/js/common/store/configureStore.js
@@ -1,5 +1,4 @@
 /* @flow */
-/* eslint global-require: 0 */ // webpack cannot drop unused dependencies
 
 import {applyMiddleware, compose, createStore} from 'redux'
 import {batchedSubscribe} from 'redux-batched-subscribe'
@@ -11,12 +10,6 @@ const middlewares: Function[] = [
   multi,
   thunk
 ]
-
-if (process.env.NODE_ENV === 'development') {
-  const createLogger: Function = require('redux-logger')
-
-  middlewares.push(createLogger()) // must be the last item
-}
 
 export default compose(
   applyMiddleware(...middlewares),
