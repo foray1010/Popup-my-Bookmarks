ca8387610ea7d3c87d666033e35c2af989e5824a
foray1010 <foray1010@users.noreply.github.com>
2017-09-25 10:29:47 +0800

    Clearer webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -16,196 +16,189 @@ const ZipPlugin = require('zip-webpack-plugin')
 
 const pkg = require('./package')
 
-const mergeAndConcat = R.mergeDeepWith(R.concat)
-
-let webpackConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader'
-      },
-      {
-        test: /\.woff$/,
-        loader: 'file-loader',
-        options: {
-          name: 'fonts/[name].[ext]'
-        }
-      },
-      {
-        test: /\.png$/,
-        loader: 'file-loader',
-        options: {
-          name: 'img/[name].[ext]'
-        }
-      },
-      {
-        test: /\/manifest\.yml$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].json'
-            }
-          },
-          'extract-loader',
-          {
-            loader: 'chrome-manifest-loader',
-            options: {
-              mapVersion: true
-            }
-          },
-          'yaml-loader'
-        ]
-      }
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, outputDir),
-    filename: path.join('js', '[name].js')
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDir]),
-    new CopyWebpackPlugin([
-      {
-        context: sourceDir,
-        from: path.join('_locales', '*', '*.json')
-      },
-      {
-        from: 'LICENSE'
-      }
-    ]),
-    new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'defer'
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.CommonsChunkPlugin(commonChunkName),
-    new webpack.optimize.OccurrenceOrderPlugin(true)
-  ],
-  resolve: {
-    alias: {
-      'seamless-immutable': 'seamless-immutable/src/seamless-immutable',
-      store: 'store/dist/store.modern'
-    },
-    extensions: ['.js', '.jsx']
-  }
-}
-
-appNames.forEach((appName) => {
-  webpackConfig = mergeAndConcat(webpackConfig, {
-    entry: {
-      [appName]: `./${sourceDir}/js/${appName}`
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        chunks: [commonChunkName, appName],
-        filename: `${appName}.html`,
-        inject: 'body',
-        template: path.join(sourceDir, 'html', 'common', 'layout.pug'),
-        title: pkg.name
-      })
-    ]
-  })
-})
+const getMergedConfigByEnv = R.converge(R.mergeDeepWith(R.concat), [
+  R.prop('default'),
+  R.propOr({}, process.env.NODE_ENV)
+])
 
 const cssLoaderOptions = {
   modules: true,
   importLoaders: 1,
   localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
 }
-switch (process.env.NODE_ENV) {
-  case 'development':
-    webpackConfig = mergeAndConcat(webpackConfig, {
-      devtool: 'source-map',
-      module: {
-        rules: [
-          {
-            test: /\.css$/,
+
+const webpackConfig = getMergedConfigByEnv({
+  default: {
+    entry: R.reduce((acc, appName) => {
+      return R.merge(acc, {
+        [appName]: `./${sourceDir}/js/${appName}`
+      })
+    }, {}, appNames),
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-loader'
+        },
+        {
+          test: /\.woff$/,
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]'
+          }
+        },
+        {
+          test: /\.png$/,
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[ext]'
+          }
+        },
+        {
+          test: /\/manifest\.yml$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].json'
+              }
+            },
+            'extract-loader',
+            {
+              loader: 'chrome-manifest-loader',
+              options: {
+                mapVersion: true
+              }
+            },
+            'yaml-loader'
+          ]
+        }
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, outputDir),
+      filename: path.join('js', '[name].js')
+    },
+    plugins: [
+      new CleanWebpackPlugin([outputDir]),
+      new CopyWebpackPlugin([
+        {
+          context: sourceDir,
+          from: path.join('_locales', '*', '*.json')
+        },
+        {
+          from: 'LICENSE'
+        }
+      ]),
+      ...R.map((appName) => {
+        return new HtmlWebpackPlugin({
+          chunks: [commonChunkName, appName],
+          filename: `${appName}.html`,
+          inject: 'body',
+          template: path.join(sourceDir, 'html', 'common', 'layout.pug'),
+          title: pkg.name
+        })
+      }, appNames),
+      new ScriptExtHtmlWebpackPlugin({
+        defaultAttribute: 'defer'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }),
+      new webpack.optimize.CommonsChunkPlugin(commonChunkName),
+      new webpack.optimize.OccurrenceOrderPlugin(true)
+    ],
+    resolve: {
+      alias: {
+        'seamless-immutable': 'seamless-immutable/src/seamless-immutable',
+        store: 'store/dist/store.modern'
+      },
+      extensions: ['.js', '.jsx']
+    }
+  },
+  development: {
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: cssLoaderOptions
+            },
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /\/icon.+\.png$/,
+          loader: 'image-process-loader',
+          options: {
+            greyscale: true
+          }
+        }
+      ]
+    },
+    plugins: [
+      new DashboardPlugin()
+    ],
+    watch: true
+  },
+  production: {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            publicPath: '../',
             use: [
-              'style-loader',
               {
                 loader: 'css-loader',
                 options: cssLoaderOptions
               },
               'postcss-loader'
             ]
-          },
-          {
-            test: /\/icon.+\.png$/,
-            loader: 'image-process-loader',
-            options: {
-              greyscale: true
-            }
-          }
-        ]
-      },
-      plugins: [
-        new DashboardPlugin()
-      ],
-      watch: true
-    })
-    break
-
-  case 'production':
-    webpackConfig = mergeAndConcat(webpackConfig, {
-      module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              publicPath: '../',
-              use: [
-                {
-                  loader: 'css-loader',
-                  options: cssLoaderOptions
-                },
-                'postcss-loader'
-              ]
-            })
-          }
-        ]
-      },
-      plugins: [
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          reportFilename: path.join('..', '__report.html')
-        }),
-        new ExtractTextPlugin({
-          filename: path.join('css', '[name].css'),
-          allChunks: true
-        }),
-        new webpack.LoaderOptionsPlugin({
-          minimize: true
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          compress: {
-            drop_console: true,
-            pure_getters: true,
-            unsafe: true,
-            warnings: false
-          },
-          output: {
-            comments: false,
-            screw_ie8: true
-          }
-        }),
-        new OptimizeJsPlugin(),
-        new ZipPlugin({
-          filename: `${pkg.version}.zip`
-        })
+          })
+        }
       ]
-    })
-    break
-
-  default:
-}
+    },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: path.join('..', '__report.html')
+      }),
+      new ExtractTextPlugin({
+        filename: path.join('css', '[name].css'),
+        allChunks: true
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          drop_console: true,
+          pure_getters: true,
+          unsafe: true,
+          warnings: false
+        },
+        output: {
+          comments: false,
+          screw_ie8: true
+        }
+      }),
+      new OptimizeJsPlugin(),
+      new ZipPlugin({
+        filename: `${pkg.version}.zip`
+      })
+    ]
+  }
+})
 
 module.exports = webpackConfig
