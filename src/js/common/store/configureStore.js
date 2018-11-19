60c53923a9f7e553727c6c78bccb4f5d6e4916a9
foray1010 <foray1010@users.noreply.github.com>
2018-11-19 17:59:40 +0800

    chore: improve configureStore type

--- src/js/common/store/configureStore.js
+++ src/js/common/store/configureStore.js
@@ -1,16 +1,19 @@
 // @flow strict
 
 import {applyMiddleware, createStore} from 'redux'
-import type {CombinedReducer} from 'redux'
+import type {Reducer, Store} from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import type {Saga} from 'redux-saga'
 
-type Payload = {|
-  preloadedState?: Object,
-  rootReducer: CombinedReducer<any, any>,
+export default <S, A>({
+  preloadedState,
+  rootReducer,
+  rootSaga
+}: {|
+  preloadedState?: S,
+  rootReducer: Reducer<S, A>,
   rootSaga: () => Saga<void>
-|}
-export default ({rootReducer, rootSaga, preloadedState = {}}: Payload) => {
+|}): Store<S, A> => {
   const sagaMiddleware = createSagaMiddleware()
 
   const store = createStore(rootReducer, preloadedState, applyMiddleware(sagaMiddleware))
