88f012a2eb843580431589a401ce9aaa8115c4c6
Alex Young <foray1010@users.noreply.github.com>
2019-02-12 18:34:22 +0800

    refactor: improve flow coverage

--- src/js/common/utils/normalizeInputtingValue.js
+++ src/js/common/utils/normalizeInputtingValue.js
@@ -1,8 +1,5 @@
 // @flow strict
 
-import * as R from 'ramda'
-
-export default R.compose(
-  R.replace(/\s+/g, ' '),
-  (value: string) => value.trimLeft()
-)
+export default (value: string) => {
+  return value.trimLeft().replace(/\s+/g, ' ')
+}
