9b8f1145a3817b9d2cfd66e3da60acbdb2feae2d
Alex Young <foray1010@users.noreply.github.com>
2019-04-30 22:37:32 +0800

    refactor: improve typescript coverage on generateLocalesFromTransifex

--- scripts/generateLocalesFromTransifex.ts
+++ scripts/generateLocalesFromTransifex.ts
@@ -2,89 +2,94 @@ import path from 'path'
 
 import bluebird from 'bluebird'
 import fs from 'fs-extra'
-// @ts-ignore
-import prompt from 'prompt'
+import prompts from 'prompts'
 import R from 'ramda'
-// @ts-ignore
 import Transifex from 'transifex'
 
-bluebird.promisifyAll(prompt)
-
-const schema = {
-  properties: {
-    transifex_username: {
-      description: 'transifex_username (you can register one for free)',
-      required: true
-    },
-    transifex_password: {
-      hidden: true,
-      required: true
-    }
+const questions: Array<prompts.PromptObject<string>> = [
+  {
+    type: 'text',
+    name: 'transifex_username',
+    message: 'transifex_username (you can register one for free)',
+    validate: Boolean
+  },
+  {
+    type: 'password',
+    name: 'transifex_password',
+    message: 'password',
+    validate: Boolean
   }
-}
+]
 
 const projectSlug = 'popup-my-bookmarks'
 const resourceSlug = 'messagesjson-1'
 
 const localesPath = path.join('src', '_locales')
-;(async () => {
-  try {
-    prompt.start()
-    const cred = await prompt.getAsync(schema)
 
-    const transifex = new Transifex({
-      credential: `${cred.transifex_username}:${cred.transifex_password}`
-    })
-    bluebird.promisifyAll(transifex)
-
-    const availableLanguages = await transifex.statisticsMethodsAsync(projectSlug, resourceSlug)
-    await Promise.all(
-      Object.keys(availableLanguages).map(async (availableLanguage) => {
-        const messagesJsonStr = await transifex.translationInstanceMethodAsync(
-          projectSlug,
-          resourceSlug,
-          availableLanguage,
-          {mode: 'onlytranslated'}
-        )
-        const messagesJson = JSON.parse(messagesJsonStr)
-
-        const sortedMessagesJson = Object.keys(messagesJson)
-          .filter((key) => Boolean(R.path([key, 'message'], messagesJson)))
-          .sort()
-          .reduce((obj, key) => {
-            // trim message
-            if (messagesJson[key].message) {
-              messagesJson[key].message = messagesJson[key].message.trim()
-            }
-
-            return {
-              ...obj,
-              [key]: messagesJson[key]
-            }
-          }, {})
-
-        let mappedLanguage
-        switch (availableLanguage) {
-          case 'nb_NO':
-            mappedLanguage = 'nb'
-            break
-
-          default:
-            mappedLanguage = availableLanguage
-        }
-
-        await fs.mkdirs(path.join(localesPath, mappedLanguage))
-
-        await fs.outputJson(
-          path.join(localesPath, mappedLanguage, 'messages.json'),
-          sortedMessagesJson,
-          {spaces: 2}
-        )
-
-        console.log(`"${mappedLanguage}" is generated`)
-      })
-    )
-  } catch (err) {
-    console.error(err.stack)
+interface Messages {
+  [key: string]: {
+    message: string
+    description?: string
   }
-})()
+}
+
+const main = async () => {
+  const response = await prompts(questions, {onCancel: () => process.exit(130)})
+
+  const transifex = new Transifex({
+    credential: `${response.transifex_username}:${response.transifex_password}`
+  })
+  bluebird.promisifyAll(transifex)
+
+  const availableLanguages = await transifex.statisticsMethodsAsync(projectSlug, resourceSlug)
+  await Promise.all(
+    Object.keys(availableLanguages).map(async (availableLanguage) => {
+      const messagesJsonStr = await transifex.translationInstanceMethodAsync(
+        projectSlug,
+        resourceSlug,
+        availableLanguage,
+        {mode: 'onlytranslated'}
+      )
+      const messagesJson: Messages = JSON.parse(messagesJsonStr)
+
+      const sortedMessagesJson = Object.keys(messagesJson)
+        .filter((key) => Boolean(R.path([key, 'message'], messagesJson)))
+        .sort()
+        .reduce((obj: Messages, key) => {
+          // trim message
+          if (messagesJson[key].message) {
+            messagesJson[key].message = messagesJson[key].message.trim()
+          }
+
+          return {
+            ...obj,
+            [key]: messagesJson[key]
+          }
+        }, {})
+
+      let mappedLanguage = ''
+      switch (availableLanguage) {
+        case 'nb_NO':
+          mappedLanguage = 'nb'
+          break
+
+        default:
+          mappedLanguage = availableLanguage
+      }
+
+      await fs.mkdirs(path.join(localesPath, mappedLanguage))
+
+      await fs.outputJson(
+        path.join(localesPath, mappedLanguage, 'messages.json'),
+        sortedMessagesJson,
+        {spaces: 2}
+      )
+
+      console.log(`"${mappedLanguage}" is generated`)
+    })
+  )
+}
+
+main().catch((err: Error) => {
+  console.error(err)
+})
---
+++ src/js/common/types/transifex.d.ts
@@ -0,0 +1,22 @@
+declare module 'transifex' {
+  interface Transifex {
+    // eslint-disable-next-line @typescript-eslint/no-misused-new
+    new (options: {credential: string, project_slug?: string}): Transifex
+
+    statisticsMethodsAsync(
+      projectSlug: string,
+      resourceSlug: string,
+      languageCode?: string
+    ): Promise<Array<string>>
+
+    translationInstanceMethodAsync(
+      projectSlug: string,
+      resourceSlug: string,
+      availableLanguage: string,
+      options?: {mode?: string}
+    ): Promise<string>
+  }
+
+  const transifex: Transifex
+  export default transifex
+}
