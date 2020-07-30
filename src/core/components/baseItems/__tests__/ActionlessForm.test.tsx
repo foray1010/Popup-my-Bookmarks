78682552ceaadc2377392bc2f4cffd7f11f38673
Alex Young <foray1010@users.noreply.github.com>
2020-07-30 20:56:29 +0800

    test: test ActionlessForm & improve Button test

---
+++ src/core/components/baseItems/__tests__/ActionlessForm.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import * as React from 'react'
+
+import ActionlessForm from '../ActionlessForm'
+
+describe('ActionlessForm', () => {
+  it('should prevent default form submit action', () => {
+    const handleSubmit = jest.fn<void, [React.FormEvent<HTMLFormElement>]>(
+      (evt) => {
+        evt.persist()
+      },
+    )
+
+    render(
+      <ActionlessForm onSubmit={handleSubmit}>
+        <button type='submit' />
+      </ActionlessForm>,
+    )
+
+    userEvent.click(screen.getByRole('button'))
+
+    expect(handleSubmit).toBeCalledWith(
+      expect.objectContaining({
+        defaultPrevented: true,
+      }),
+    )
+  })
+})
--- src/core/components/baseItems/__tests__/Button.test.tsx
+++ src/core/components/baseItems/__tests__/Button.test.tsx
@@ -2,19 +2,19 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import * as React from 'react'
 
-import Button from '../index'
+import Button from '../Button'
 
 describe('Button', () => {
   it('should not fire form submit when clicked in form', () => {
-    const handleSubmit = jest.fn()
+    const handleSubmit = jest.fn<void, [React.FormEvent<HTMLFormElement>]>()
 
     render(
       <form onSubmit={handleSubmit}>
-        <Button data-testid='button' />
+        <Button />
       </form>,
     )
 
-    userEvent.click(screen.getByTestId('button'))
+    userEvent.click(screen.getByRole('button'))
 
     expect(handleSubmit).not.toBeCalled()
   })
