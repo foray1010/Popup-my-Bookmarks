2fe7e403bffe03ec29b197196e9e2cf024c1bc97
Alex Young <foray1010@users.noreply.github.com>
2020-08-04 09:58:03 +0800

    refactor: rewrite useKeyBindingsEvent with use-deep-compare

--- src/popup/components/keyBindings/useKeyBindingsEvent.ts
+++ src/popup/components/keyBindings/useKeyBindingsEvent.ts
@@ -1,29 +1,21 @@
 import * as React from 'react'
+import { useDeepCompareEffect } from 'use-deep-compare'
 
 import type { KeyBindingMeta } from './KeyBindingsContext'
 import { useKeyBindingsContext } from './KeyBindingsContext'
 
 const useKeyBindingsEvent = (
-  { key, priority, windowId }: KeyBindingMeta,
-  callback?: (evt: KeyboardEvent) => void,
+  meta: KeyBindingMeta,
+  callback: (evt: KeyboardEvent) => void,
 ) => {
   const { addEventListener, removeEventListener } = useKeyBindingsContext()
 
   const callbackRef = React.useRef(callback)
   callbackRef.current = callback
 
-  // use ref as `key` may be a RegExp instance and passing new reference every time
-  // it made a trade-off, updating key now doesn't rerun addEventListener/removeEventListener
-  const keyRef = React.useRef(key)
-  keyRef.current = key
-
-  React.useEffect(() => {
-    const meta = { key: keyRef.current, priority, windowId }
-
+  useDeepCompareEffect(() => {
     const wrappedCallback = (evt: KeyboardEvent) => {
-      if (callbackRef.current) {
-        callbackRef.current(evt)
-      }
+      callbackRef.current(evt)
     }
 
     addEventListener(meta, wrappedCallback)
@@ -31,7 +23,7 @@ const useKeyBindingsEvent = (
     return () => {
       removeEventListener(meta, wrappedCallback)
     }
-  }, [addEventListener, priority, removeEventListener, windowId])
+  }, [addEventListener, meta, removeEventListener])
 }
 
 export default useKeyBindingsEvent
