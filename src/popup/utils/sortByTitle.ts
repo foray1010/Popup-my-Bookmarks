b41685d3812a7592d3659956d4234ab7a6d77003
Alex Young <foray1010@users.noreply.github.com>
2021-03-18 20:11:34 +0800

    feat: natural sort in search result and when sort bookmark by name

--- src/popup/utils/sortByTitle.ts
+++ src/popup/utils/sortByTitle.ts
@@ -3,7 +3,10 @@ import type { BookmarkInfo } from '../types'
 export default function sortByTitle(
   bookmarkInfos: BookmarkInfo[],
 ): BookmarkInfo[] {
-  const collator = new Intl.Collator()
+  const collator = new Intl.Collator(undefined, {
+    numeric: true,
+    sensitivity: 'base',
+  })
   return Array.from(bookmarkInfos).sort((a, b) =>
     collator.compare(a.title, b.title),
   )
