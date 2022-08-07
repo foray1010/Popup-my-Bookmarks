0455e8581d446e67813366709aa65671dec4e26a
Alex Young <foray1010@users.noreply.github.com>
2022-08-07 19:56:06 +0100

    chore: use root: true in eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,6 @@
 'use strict'
 
 module.exports = {
+  root: true,
   extends: ['@foray1010/eslint-config'],
-  rules: {
-    // https://github.com/import-js/eslint-plugin-import/issues/1739
-    'import/no-unresolved': ['error', { ignore: ['\\?'] }],
-  },
 }
--- src/.eslintrc.cjs
+++ src/.eslintrc.cjs
@@ -1,3 +1,8 @@
 module.exports = {
+  root: true,
   extends: ['@foray1010/eslint-config/react'],
+  rules: {
+    // https://github.com/import-js/eslint-plugin-import/issues/1739
+    'import/no-unresolved': ['error', { ignore: ['\\?'] }],
+  },
 }
