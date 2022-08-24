6a633afd836ca989eb6b5301fe875915de99060b
Alex Young <foray1010@users.noreply.github.com>
2022-08-24 12:51:15 +0100

    chore: use @foray1010/jest-preset

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,13 @@
 'use strict'
 
 const config = {
-  bail: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{cjs,cts,js,mjs,mts,ts,tsx}'],
-  coveragePathIgnorePatterns: ['/__fixtures__/'],
-  coverageReporters: ['lcov', 'text-summary'],
+  preset: '@foray1010',
   moduleNameMapper: {
     '\\.png$': '<rootDir>/__mocks__/fileMock.ts',
     '\\.svg$': '<rootDir>/__mocks__/fileMock.ts',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['**/*.{spec,test}.{cjs,cts,js,mjs,mts,ts,tsx}'],
   transform: {
     '\\.css$': 'jest-css-modules-transform',
     '\\.tsx?$': '@swc/jest',
