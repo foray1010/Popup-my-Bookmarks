97f3a75fb3a6d7002e20ae6227ce1bf2aa5d2344
Alex Young <foray1010@users.noreply.github.com>
2023-03-09 22:14:23 +0000

    feat: show higher resolution for bookmark favicon

--- src/popup/utils/faviconUrl.ts
+++ src/popup/utils/faviconUrl.ts
@@ -3,5 +3,6 @@ import webExtension from 'webextension-polyfill'
 export function faviconUrl(pageUrl: string) {
   const url = new URL(webExtension.runtime.getURL('/_favicon/'))
   url.searchParams.set('pageUrl', pageUrl)
+  url.searchParams.set('size', '32')
   return url.toString()
 }
