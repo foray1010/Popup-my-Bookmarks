43445255c66a934e94c0681a4fade11a305dce0f
Alex Young <foray1010@users.noreply.github.com>
2023-09-30 13:15:18 +0800

    chore: fix stylelint errors
    BREAKING CHANGE: require chrome version >= 108

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -2,4 +2,18 @@
 
 module.exports = {
   extends: ['@foray1010/stylelint-config'],
+  rules: {
+    'plugin/no-unsupported-browser-features': [
+      true,
+      {
+        ignore: [
+          // We are not using button with `display: contents`
+          'css-display-contents',
+          // Handled by lightningcss
+          'css-nesting',
+        ],
+        severity: 'error',
+      },
+    ],
+  },
 }
