b470b917b3167570f1f989d0083555a42da8d018
Alex Young <foray1010@users.noreply.github.com>
2024-11-01 10:39:09 +0800

    chore(deps): update all dependencies

--- src/core/utils/isMac.ts
+++ src/core/utils/isMac.ts
@@ -1,5 +1,5 @@
 export default function isMac() {
   // use `navigator.userAgentData.platform` in the future when all browsers support it
   // eslint-disable-next-line @typescript-eslint/no-deprecated
-  return window.navigator.platform.startsWith('Mac')
+  return globalThis.navigator.platform.startsWith('Mac')
 }
